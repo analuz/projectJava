77af9582811af5f14eae17852104db6079c6db51
Analuz <alpe2607@hotmail.com>
2012-10-06 12:36:47 -0500

    Primer commit de proyecto JAVA y Eclipse

---
+++ src/TodosLosTest.java
@@ -0,0 +1,7 @@
+public class TodosLosTest {
+	public static junit.framework.TestSuite suite(){
+		junit.framework.TestSuite suite = new junit.framework.TestSuite();
+		suite.addTestSuite(EstudianteTest.class);
+		suite.addTestSuite(SesionCursoTest.class);
+	}
+}
